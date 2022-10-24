1042f32d3972d3c8f72e260ffb6da86e6c1cd021
Francisco Emilio Cavenaghi <franchucave@hotmail.com>
2022-10-24 15:26:00 -0300

    Improve movable obstacle movement

--- Assets/Scripts/MovableObstacleBehaviour.cs
+++ Assets/Scripts/MovableObstacleBehaviour.cs
@@ -7,8 +7,15 @@ public class MovableObstacleBehaviour : MonoBehaviour
     [SerializeField] private Transform initPos;
     [SerializeField] private Transform endPos;
     [SerializeField] private GameObject obstacle;
+    [SerializeField] private float minDuration = 10;
+    [SerializeField] private float maxDuration = 20;
     [SerializeField] private float duration;
     private float lerpTime = 0;
+
+    private void Start()
+    {
+        duration = Random.Range(minDuration, maxDuration);
+    }
 
     private void Update()
     {
