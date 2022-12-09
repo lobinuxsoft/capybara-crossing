ff2fa8c47f11bfd832072999f67e01f4cdecc4a3
Lobinux <lobinux@digilearnnials.com>
2022-12-09 17:26:42 -0300

    Wwise bug fix problem

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,12 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID GAMEOVER = 4158285989U;
+        static const AkUniqueID MUSIC_MENU = 1598298728U;
+        static const AkUniqueID MX = 1685527054U;
         static const AkUniqueID PLAY_CAR_CRASH = 2838070904U;
         static const AkUniqueID PLAY_CAR_ENGINE = 2832155465U;
+        static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
         static const AkUniqueID PLAY_FREEZE = 1505496975U;
         static const AkUniqueID PLAY_HONK = 2486471960U;
         static const AkUniqueID PLAY_MENU_BACK_CLICK = 113434238U;
@@ -25,8 +29,52 @@ namespace AK
         static const AkUniqueID PLAY_MENU_PLAY = 3198441892U;
         static const AkUniqueID PLAY_RESURRECTION = 895541995U;
         static const AkUniqueID PLAY_SLOW_MO = 1400896344U;
+        static const AkUniqueID PLAY_SPLASH = 3948925255U;
         static const AkUniqueID PLAY_TELEPORT = 3785065891U;
+        static const AkUniqueID PLAY_VOX = 2452511941U;
+        static const AkUniqueID PLAY_WOOSH = 1725216094U;
+        static const AkUniqueID STEM1 = 1232066327U;
+        static const AkUniqueID STEM2 = 1232066324U;
+        static const AkUniqueID STEM3 = 1232066325U;
+        static const AkUniqueID STEM4 = 1232066322U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID LEAF = 54442137U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+        namespace MX
+        {
+            static const AkUniqueID GROUP = 1685527054U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GAMEOVER = 4158285989U;
+                static const AkUniqueID MENU = 2607556080U;
+                static const AkUniqueID STEM1 = 1232066327U;
+                static const AkUniqueID STEM2 = 1232066324U;
+                static const AkUniqueID STEM3 = 1232066325U;
+                static const AkUniqueID STEM4 = 1232066322U;
+            } // namespace SWITCH
+        } // namespace MX
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID MUSICVOLUME = 2346531308U;
+        static const AkUniqueID SFXVOLUME = 988953028U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
@@ -37,6 +85,8 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC_BUS = 3127962312U;
+        static const AkUniqueID SFX_BUS = 1502772432U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
